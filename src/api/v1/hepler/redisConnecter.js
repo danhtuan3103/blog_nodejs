1587bbf8ce7f447c9bd8690ec83bcba3cdf29820
danhtuan3103 <danhtuan3103@gmail.com>
2023-03-10 02:15:21 +0900

    first-commit

--- src/api/v1/hepler/redisConnecter.js
+++ src/api/v1/hepler/redisConnecter.js
@@ -1,21 +1,25 @@
-const redis = require('redis');
+const redis = require("redis");
 
-const client = redis.createClient('');
+const client = redis.createClient({
+  socket: {
+    host: "redis-18022.c292.ap-southeast-1-1.ec2.cloud.redislabs.com",
+    port: "18022",
+  },
+  password: 'Jbo14aXB23QslJFRbEeq9KK6VY324TNa'
+});
 
-function connect () {
-    client.connect();
+function connect() {
+  client.connect();
 
-    client.on('error', (err) => console.log('Redis Client Error', err));
-    
-    client.on('connect', (err) => {
-        console.log("Redis connected");
-    })
-    
-    client.on('ready', (err) => {
-        console.log("Redis Ready");
-    })
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  client.on("connect", (err) => {
+    console.log("Redis connected");
+  });
+
+  client.on("ready", (err) => {
+    console.log("Redis Ready");
+  });
 }
 
-
-
-module.exports = {connect, client};
+module.exports = { connect, client };
